1ed39d0dce849d138e085b176f1878db4aa5b2c7
“leeon” <499053596@qq.com>
2019-12-10 16:47:49 +0800

    修改arduino控制器

--- arduino/XRobot/XRobot.ino
+++ arduino/XRobot/XRobot.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoJson.h>
 
+int i = 0;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  DynamicJsonDocument doc(200);
+  doc = receiveCommand();
+  serializeJson(doc, Serial);
+  i = i + 1;
+  Serial.println(i);
+}
 
+DynamicJsonDocument receiveCommand() {
+  String command = "";
+  DynamicJsonDocument doc(200);
+
+  while (true) {
+    if ( Serial.available())
+    {
+      char commandChar = char(Serial.read());
+      delay(2);
+
+      if (commandChar == '#') {
+        command = "";
+        continue;
+      }
+
+      if (command.length() > 200) {
+        command = "";
+        Serial.println("this command is to long!");
+        continue;
+      } else {
+        command += commandChar;
+      }
+
+      DeserializationError error = deserializeJson(doc, command);
+
+      if (!error) {
+        return doc;
+      }
+    }
+  }
 }
