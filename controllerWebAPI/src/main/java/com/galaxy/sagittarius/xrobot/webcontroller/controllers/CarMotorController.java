bd6fa308d87d330ffb999db3b8ca52398bbbba0c
lijinbo <lilinghao1986@126.com>
2019-12-23 14:15:11 +0800

    1

--- controllerWebAPI/src/main/java/com/galaxy/sagittarius/xrobot/webcontroller/controllers/CarMotorController.java
+++ controllerWebAPI/src/main/java/com/galaxy/sagittarius/xrobot/webcontroller/controllers/CarMotorController.java
@@ -5,6 +5,7 @@ import com.galaxy.sagittarius.xrobot.webcontroller.ServialCommandHelper;
 import com.galaxy.sagittarius.xrobot.webcontroller.domain.Motor;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,20 +20,17 @@ public class CarMotorController {
         motor.setDeviceModel("370");
         motor.setDeviceNumber(1);
         motor.setDirection(0);
-        motor.setSpeed(30);
+        motor.setSpeed(0);
     }
 
     /**
      * 前进
      */
     @ApiOperation(value = "前进", notes = "前进")
-    @ApiResponses({
-            @ApiResponse(code = 500, message = "服务器内部错误"),
-            @ApiResponse(code = 404, message = "找不到请求路径")
-    })
-    @GetMapping("/go")
-    public JSONObject go() {
+    @GetMapping("/go/{speed}")
+    public JSONObject go(@PathVariable(name = "speed") int speed) {
         motor.setDirection(1);
+        motor.setSpeed(speed);
         JSONObject commandJson = (JSONObject) JSONObject.toJSON(motor);
         ServialCommandHelper.SendData(commandJson.toJSONString());
         return commandJson;
@@ -41,9 +39,11 @@ public class CarMotorController {
     /**
      * 后退
      */
-    @GetMapping("/back")
-    public JSONObject back() {
+    @ApiOperation(value = "后退", notes = "后退")
+    @GetMapping("/back/{speed}")
+    public JSONObject back(@PathVariable(name = "speed") int speed) {
         motor.setDirection(-1);
+        motor.setSpeed(speed);
         JSONObject commandJson = (JSONObject) JSONObject.toJSON(motor);
         ServialCommandHelper.SendData(commandJson.toJSONString());
         return commandJson;
@@ -52,9 +52,11 @@ public class CarMotorController {
     /**
      * 停止
      */
+    @ApiOperation(value = "停止", notes = "停止")
     @GetMapping("/stop")
     public JSONObject stop() {
         motor.setDirection(0);
+        motor.setSpeed(0);
         JSONObject commandJson = (JSONObject) JSONObject.toJSON(motor);
         ServialCommandHelper.SendData(commandJson.toJSONString());
         return commandJson;
